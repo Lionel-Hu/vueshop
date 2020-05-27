c89a1786bdb079a3021a1c7fb535f4e299262b1f
jason <1669751664@qq.com>
2020-05-27 14:57:59 +0800

    登录功能的实现

---
+++ src/components/Login.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="login_container">
+        <!-- 登录框 -->
+        <div class="login_box">
+            <!-- 登录头像框 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <!-- 登录表单 -->
+                <el-form ref="loginFormRef" label-width="0px" :model="loginForm" :rules="loginFormRules" class="login_form">
+                    <!-- 用户名输入框 -->
+                    <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+                    </el-form-item>
+                    <!-- 密码输入框 -->
+                    <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                    </el-form-item>
+                    <!-- 按钮区域 -->
+                    <el-form-item class="btns">
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                    </el-form-item>
+                </el-form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // 登录表单对象
+            loginForm:{
+                username:'admin',
+                password:'123456'  
+            },
+            // 表单验证规则
+            loginFormRules:{
+                // 验证用户名是否匹配
+                username:[{
+                    required: true, message: '请输入用户名', trigger: 'blur' 
+                },
+                {
+                    min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'
+                }],
+                password:[{
+                    required: true, message: '请输入密码', trigger: 'blur' 
+                },
+                {
+                    min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'
+                }]
+            }
+        }
+    },
+    methods:{
+        // 表单重置按钮的实现
+        resetLoginForm(){
+            // console.log(this);
+            this.$refs.loginFormRef.resetFields();
+        },
+        // 表单登录预验证,valid是一个布尔值
+        login(){
+            this.$refs.loginFormRef.validate(async valid=>{
+                // console.log(valid);
+                if(!valid)return;
+               const {data:res}= await this.$http.post("login",this.loginForm);
+            //    console.log(res);
+            if (res.meta.status!==200) {
+                // return console.log('登录失败');
+                return this.$message.error('登陆失败,用户名或者密码错误');
+                                
+            }
+            // else console.log('登录成功');
+            else this.$message.success('登录成功');
+               window.sessionStorage.setItem('token',res.data.token);
+               this.$router.push('/home');
+            });
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+// 全局背景添加样式
+.login_container{
+    background-color :#2b4b6b;
+    height: 100%;
+}
+// 登录页面盒子添加样式
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);//水平，垂直居中
+    // 利用less语法，进行样式设置
+    .avatar_box{
+        width: 130px;
+        height: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #dddddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background-color: #fff;
+        img{
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+// 表单输入框样式
+
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+
+// 表单按钮样式
+.btns{
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+    routes: [{
+            path: '/',
+            redirect: '/login'
+        },
+        { path: '/login', component: Login },
+        { path: '/Home', component: Home }
+    ]
 })
 
-export default router
+// 添加路由导航守卫
+router.beforeEach((to, from, next) => {
+    // to表示将要访问的路径
+    // from 代表从哪个路径跳转而来
+    // next 是一个函数,表示放行
+    // next()直接放行, next('/路径')强制跳转到特定的路径
+    if (to.path === '/login')
+        return next();
+    const tokenStr = window.sessionStorage.getItem('token');
+    if (!tokenStr) return next('/login');
+    next();
+
+});
+export default router;
